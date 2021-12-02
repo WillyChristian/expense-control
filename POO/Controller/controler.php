90d3d7af8eeca364f5935473fd2f32e418cc8ee4
WillyChristian <crisaort@gmail.com>
2021-12-02 00:28:42 -0300

    Adding composer to maintain dependecies

---
+++ POO/Controller/controler.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace controller; //identificação da classe, para caso tenham mais de uma com o mesmo nome
+
+class Produto
+{
+  public function Origin()
+  {
+    echo "Vindo de dentro da Controler";
+  }
+}
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+
+require __DIR__ . "/POO/Model/model.php";
+require __DIR__ . "/POO/Controller/controler.php";
+
+// \nome\ defini a origem pelo namespace
+$produto = new \controller\Produto();
+$produto->Origin();
